c2c183f740e1f7fdd42c2580a00f1b72c7fb5f42
pineoc <pineoc@naver.com>
2013-09-02 03:51:09 +0900

    pauseGameScene edit

--- HungryGame/proj.win32/PauseGameScene.cpp
+++ HungryGame/proj.win32/PauseGameScene.cpp
@@ -1,11 +1,99 @@
 #include "PauseGameScene.h"
+#include "mainScene.h"
+#include "HelpScene.h"
 
+using namespace cocos2d;
 
-PauseGameScene::PauseGameScene(void)
+CCScene* PauseGameScene::scene()
 {
+	CCScene* scene = CCScene::create();
+
+	PauseGameScene *layer = PauseGameScene::create();
+
+	scene->addChild(layer);
+
+	return scene;
+}
+
+bool PauseGameScene::init()
+{
+	if( !CCLayerColor::initWithColor(ccc4(0,0,0,0)))
+	{ return false; }
+	
+	CCString* popParam=CCString::create("0");
+	CCNotificationCenter::sharedNotificationCenter()->postNotification("notification", popParam);         //노티피케이션 보내기
+
+	winSize=CCDirector::sharedDirector()->getWinSize();
+
+	CCSprite *bg = CCSprite::create("img\\pause\\pause_bg.png");
+	bg->setPosition(ccp(winSize.width/2,winSize.height/2));
+	this->addChild(bg,0);
+	
+	//메뉴추가
+	CCMenuItemImage* pMain = CCMenuItemImage::create(
+		"img\\pause\\pause_btn_goMain.png","img\\pause\\pause_btn_goMain_n.png", this, menu_selector(PauseGameScene::goMain));
+	CCMenuItemImage* pHelp = CCMenuItemImage::create(
+		"img\\pause\\pause_btn_help.png","img\\pause\\pause_btn_help_n.png",this,menu_selector(PauseGameScene::goHelp));
+	CCMenuItemImage* pGame = CCMenuItemImage::create(
+		"img\\pause\\pause_btn_restart.png","img\\pause\\pause_btn_restart_n.png", this, menu_selector(PauseGameScene::newGame));
+	CCMenuItemImage* pClose = CCMenuItemImage::create(
+		"img\\pause\\pause_btn_continue.png","img\\pause\\pause_btn_continue.png", this, menu_selector(PauseGameScene::doClose));
+
+	//메뉴생성
+	CCMenu* pauseMenu=CCMenu::create(pMain, pGame, pHelp, pClose, NULL);
+	pauseMenu->setPosition(ccp(240, 100));
+	this->addChild(pauseMenu,10);
+
+	//backLayer추가
+	backLayer=CCLayerColor::create(ccc4(0,0,0,0), winSize.width, winSize.height);
+	backLayer->setAnchorPoint(ccp(0,0));
+	backLayer->setPosition(ccp(0,0));
+	this->addChild(backLayer);
+
+	//popUpLayer추가
+	popUpLayer=CCLayerColor::create(ccc4(255,255,255,255), 250,150);
+	popUpLayer->setAnchorPoint(ccp(0,0));
+	popUpLayer->setPosition(ccp((winSize.width-popUpLayer->getContentSize().width)/2, 
+				(winSize.height-popUpLayer->getContentSize().height)/2,   )  );
+	this->addChild(popUpLayer);
+
+    return true;
 }
 
 
-PauseGameScene::~PauseGameScene(void)
+void PauseGameScene::goMain( CCObject* pSender )
 {
+	//처음 메인화면으로
+	CCScene *pScene = Main::scene();
+
+	CCDirector::sharedDirector()->pushScene(pScene);
+}
+
+void PauseGameScene::goHelp( CCObject* pSender )
+{
+	//도움말 화면으로
+	CCScene *pScene = HelpScene::scene();
+
+	CCDirector::sharedDirector()->pushScene(pScene);
+}
+
+void PauseGameScene::newGame( CCObject* pSender )
+{
+	//게임화면 초기화
+}
+
+void PauseGameScene::doClose(CCObject* pSender)
+{
+	//팝업창 닫고 게임 이어하기
+	CCString* popParam=CCString::create("1");
+	CCNotificationCenter::sharedNotificationCenter()->postNotification("notification", popParam);         //노티피케이션 보내기
+	this->removeFromParentAndCleanup(true);		//팝업창 제거
+
+}
+
+void PauseGameScene::menuPauseCallback (CCObject *pSender)
+{
+	CCScene *pScene = PauseGameScene::scene();
+
+	CCDirector::sharedDirector()->pushScene(pScene);
 }
--- HungryGame/proj.win32/PauseGameScene.h
+++ HungryGame/proj.win32/PauseGameScene.h
@@ -1,8 +1,37 @@
+/*지윤 - 게임 중 일시정지 창
+게임 도중 일시정지
+메인으로
+새게임
+도움말
+이어하기*/
 #pragma once
-class PauseGameScene
+#ifndef _PauseGameScene_
+#define _PauseGameScene_
+
+#include "cocos2d.h"
+
+class PauseGameScene : public cocos2d::CCLayerColor
 {
 public:
-	PauseGameScene(void);
-	~PauseGameScene(void);
+	virtual bool init();
+
+	static cocos2d::CCScene* scene();
+
+	CREATE_FUNC(PauseGameScene);
+
+	cocos2d::CCSize winSize;
+
+	CCLayerColor* backLayer;
+	CCLayerColor* popUpLayer;
+
+	//메뉴 4가지
+	void goMain( CCObject* pSender );	//메인으로
+	void newGame( CCObject* pSender );	//새게임
+	void goHelp ( CCObject* pSender );	//도움말
+	void doClose( CCObject* pSender );	//이어하기
+
+	
+	void menuPauseCallback (CCObject* pSender);	//콜백
 };
 
+#endif
